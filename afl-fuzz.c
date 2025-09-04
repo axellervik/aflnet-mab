08e7d06fb195c511539728db1d7fdbc05a45975f
Axel Lervik <70861555+axellervik@users.noreply.github.com>
2025-09-04 16:29:19 +0200

    Change log location

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -315,7 +315,7 @@ EXP3* exp3_scheduler; /* Globally available EXP3 scheduler */
 static FILE *fp_weights = NULL;
 void log_double(double val) {
     if (!fp_weights) {
-        fp_weights = fopen("weights.log", "w");
+        fp_weights = fopen("/log/weights.log", "w");
         if (!fp_weights) return;
     }
     fprintf(fp_weights, "%.17g\n", val);
@@ -9738,6 +9738,7 @@ stop_fuzzing:
   ck_free(target_path);
   ck_free(sync_id);
 
+  if (fp) fclose(fp);
   exp3_free(exp3_scheduler);
   destroy_ipsm();
   destroy_message_code_map();
